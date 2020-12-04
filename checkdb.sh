45b0b7068a3e50a7e426b25d6b95953fc42961a2
Charles Bourgeaux <ch.bourgeaux@gmail.com>
2020-12-04 17:01:43 +0100

    fix: bad interpreter for bash files

--- checkdb.sh
+++ checkdb.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sqlite3 $DIR/data.sq3 "DELETE FROM files WHERE date <= strftime('%s', datetime('now', '-1 day'));"
--- checkfiles.sh
+++ checkfiles.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 find $DIR/dist/data/ -mmin +1440 -exec rm -f {} \;
